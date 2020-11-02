060f476239a396d664c45fb585b8ece96c6cffb8
alarikshaw <337349132@qq.com>
2020-11-02 15:26:14 +0800

    gitalk

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -172,8 +172,8 @@ module.exports = {
       {
         choosen: 'gitalk',
         options: {
-          clientID: '2ae2641a8f97a43d8677',
-          clientSecret: 'a88e4e63cdf678b7f08422aceb4a226639fe6c6c',
+          clientID: '7d131de9be7d0b0402f8',
+          clientSecret: '6960a6f697ce5af31b257fac82873d7e0ba9e20e',
           repo: 'blog-gitalk-comment', // GitHub 仓库
           owner: 'Alarikshaw', // GitHub仓库所有者
           admin: ['Alarikshaw'], // 对仓库有写权限的人
